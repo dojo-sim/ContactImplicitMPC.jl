b1b07d79ef4068ba07240f8d2ae82cff31e2bc55
simon-lc <simonlc@stanford.edu>
2021-03-03 09:32:17 -0800

    update pkgs

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,12 +9,12 @@ using Ipopt
 using JLD2
 using JuMP
 using MeshCat
-using ModelingToolkit
+# using ModelingToolkit
 using NLsolve
 using Parameters
 using Plots
 using Rotations
-using SCS
+# using SCS
 using StaticArrays
 using LinearAlgebra
 using Logging
