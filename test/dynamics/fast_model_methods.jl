221746d4da49486249425f16853f178ea6256dd6
simon-lc <simonlc@stanford.edu>
2021-03-08 14:20:51 -0800

    fixing dynamics tests

--- test/dynamics/fast_model_methods.jl
+++ test/dynamics/fast_model_methods.jl
@@ -12,7 +12,8 @@
 
     # Testing fast methods
     @test norm(ContactControl.M_fast(model, q0s) - M_func(model, q0s), Inf) < 1.0e-8
-    @test norm(ContactControl.B_fast(model, q0s) - B_func(model, q0s), Inf) < 1.0e-8
+	@test norm(ContactControl.B_fast(model, q0s) - B_func(model, q0s), Inf) < 1.0e-8
+    @test norm(ContactControl.A_fast(model, q0s) - A_func(model, q0s), Inf) < 1.0e-8
     @test norm(ContactControl.N_fast(model, q0s) - N_func(model, q0s), Inf) < 1.0e-8
     @test norm(ContactControl.P_fast(model, q0s) - P_func(model, q0s), Inf) < 1.0e-8
     @test norm(ContactControl.C_fast(model, q0s, q̇0s) - C_func(model, q0s, q̇0s), Inf) < 1.0e-8
@@ -43,10 +44,11 @@ end
     b1s = rand(SizedVector{nb,T})
     q2s = rand(SizedVector{nq,T})
 
-    ∇ys   = rand(SizedMatrix{nq,2nq + nu + nc + nb + nq,T})
+    ∇ys   = rand(SizedMatrix{nq,2nq + nu + nw + nc + nb + nq,T})
     ∇q0s  = rand(SizedMatrix{nq,nq,T})
     ∇q1s  = rand(SizedMatrix{nq,nq,T})
-    ∇u1s  = rand(SizedMatrix{nq,nu,T})
+	∇u1s  = rand(SizedMatrix{nq,nu,T})
+    ∇w1s  = rand(SizedMatrix{nq,nw,T})
     ∇γ1s  = rand(SizedMatrix{nq,nc,T})
     ∇b1s  = rand(SizedMatrix{nq,nb,T})
     ∇q2s  = rand(SizedMatrix{nq,nq,T})
@@ -63,9 +65,13 @@ end
 	fq1(x) = ContactControl.dynamics(model, h, q0s, x, u1s, w1s, γ1s, b1s, q2s)
     @test norm(∇q1s - ForwardDiff.jacobian(fq1, q1s), Inf) < 1.0e-8
 
-    ContactControl.du1_fast!(∇u1s, model, h, q0s, q1s, u1s, w1s, γ1s, b1s, q2s)
+	ContactControl.du1_fast!(∇u1s, model, h, q0s, q1s, u1s, w1s, γ1s, b1s, q2s)
 	fu1(x) = ContactControl.dynamics(model, h, q0s, q1s, x, w1s, γ1s, b1s, q2s)
-    @test norm(∇u1s - ForwardDiff.jacobian(fu1, u1s), Inf) < 1.0e-8
+	@test norm(∇u1s - ForwardDiff.jacobian(fu1, u1s), Inf) < 1.0e-8
+
+	ContactControl.dw1_fast!(∇w1s, model, h, q0s, q1s, u1s, w1s, γ1s, b1s, q2s)
+	fw1(x) = ContactControl.dynamics(model, h, q0s, q1s, x, w1s, γ1s, b1s, q2s)
+	@test norm(∇w1s - ForwardDiff.jacobian(fw1, w1s), Inf) < 1.0e-8
 
     ContactControl.dγ1_fast!(∇γ1s, model, h, q0s, q1s, u1s, w1s, γ1s, b1s, q2s)
 	fγ1(x) = ContactControl.dynamics(model, h, q0s, q1s, u1s, w1s, x, b1s, q2s)
@@ -79,9 +85,8 @@ end
 	fq2(x) = ContactControl.dynamics(model, h, q0s, q1s, u1s, w1s, γ1s, b1s, x)
     @test norm(∇q2s - ForwardDiff.jacobian(fq2, q2s), Inf) < 1.0e-8
 
-    # dy_fast!(∇ys, model, h, q0s, q1s, u1s, w1s, γ1s, b1s, q2s)
-	# # @show [∇q0s ∇q1s ∇u1s ∇γ1s ∇b1s ∇q2s]
-    # @test norm(∇ys - [∇q0s ∇q1s ∇u1s ∇γ1s ∇b1s ∇q2s], Inf) < 1.0e-8
+    dy_fast!(∇ys, model, h, q0s, q1s, u1s, w1s, γ1s, b1s, q2s)
+    @test norm(∇ys - [∇q0s ∇q1s ∇u1s ∇w1s ∇γ1s ∇b1s ∇q2s], Inf) < 1.0e-8
 end
 
 @testset "Model Methods: Fast Residual (Particle)" begin
