c4bb06316eae78d3d018449a30ccaabad3bfaaca
simon-lc <simonlc@stanford.edu>
2021-03-26 14:06:06 -0700

    fixed threads demo

--- src/controller/dev_threads.jl
+++ src/controller/dev_threads.jl
@@ -1,27 +1,30 @@
 using BenchmarkTools
+using StaticArrays
 Threads.nthreads()
 
-struct LinSys{T}
-	A::Array{T, 2}
-	b::Vector{T}
-	x::Vector{T}
+mutable struct LinSys{T,n,nn}
+	A::SMatrix{n,n,T,nn}
+	b::SVector{n,T}
+	x::SVector{n,T}
 end
 
-linsys = [LinSys(rand(1000, 1000), rand(1000), zeros(1000)) for t = 1:1000]
+linsys = [LinSys(SMatrix{10,10}(rand(10, 10)), rand(SVector{10}), zeros(SVector{10})) for t = 1:1000]
 
 function solve_linsys!(s::LinSys)
-	s.x .= s.A \ s.b
+	for t = 1:10000
+		s.x = s.A \ s.b
+	end
 	nothing
 end
 
-function solve_all!(s::Vector{LinSys{T}}) where T
+function solve_all!(s::Vector{LinSys{T,n,nn}}) where {T,n,nn}
 	for ss in s
 		solve_linsys!(ss)
 	end
 	nothing
 end
 
-function solve_all_threads!(s::Vector{LinSys{T}}) where T
+function solve_all_threads!(s::Vector{LinSys{T,n,nn}}) where {T,n,nn}
 	Threads.@threads for ss in s
 		solve_linsys!(ss)
 	end
@@ -31,7 +34,11 @@ end
 @code_warntype solve_all!(linsys)
 @code_warntype solve_all_threads!(linsys)
 
+@time solve_all!(linsys)
+@time solve_all_threads!(linsys)
+
 @benchmark solve_all!($linsys)
 @benchmark solve_all_threads!($linsys)
 
-@profile solve_all_threads!(linsys)
+@profiler solve_all!(linsys)
+@profiler solve_all_threads!(linsys)
