07ae1f32f31a6be997c8915c04444e13f9f0627f
simon-lc <simonlc@stanford.edu>
2021-04-02 13:48:14 -0700

    working mpc with IP2

--- src/example/quadruped_mpc.jl
+++ src/example/quadruped_mpc.jl
@@ -23,13 +23,13 @@ ref_traj0 = deepcopy(ref_traj)
 n_opts0 = NewtonOptions(r_tol=3e-4, κ_init=κ, κ_tol=2κ, solver_inner_iter=5)
 m_opts0 = MPCOptions{T}(
             N_sample=2,
-            M=100,
+            M=600,
             H_mpc=10,
             κ=κ,
             κ_sim=1e-8,
             r_tol_sim=1e-8,
             open_loop_mpc=false,
-            w_amp=[-0.10, -0.10],
+            w_amp=[-0.10, -0.90],
             ip_max_time=0.1,
             live_plotting=false)
 cost0 = CostFunction(H, model.dim,
@@ -64,12 +64,12 @@ plot!(plt[2,1], hcat(Vector.([u[1:nu] for u in mpc0.u_sim]*m_opts0.N_sample)...)
 
 visualize!(vis, model, mpc0.q_sim[1:10:end], Δt=10*h/m_opts0.N_sample, name=:mpc)
 
-# filename = "quadruped_mpc_wind"
-# MeshCat.convert_frames_to_video(
-#     "/home/simon/Downloads/$filename.tar",
-#     "/home/simon/Documents/$filename.mp4", overwrite=true)
-#
-# convert_video_to_gif(
-#     "/home/simon/Documents/$filename.mp4",
-#     "/home/simon/Documents/$filename.gif", overwrite=true)
+filename = "quadruped_mpc_downwind"
+MeshCat.convert_frames_to_video(
+    "/home/simon/Downloads/$filename.tar",
+    "/home/simon/Documents/$filename.mp4", overwrite=true)
+
+convert_video_to_gif(
+    "/home/simon/Documents/$filename.mp4",
+    "/home/simon/Documents/$filename.gif", overwrite=true)
 11/(100*h)
