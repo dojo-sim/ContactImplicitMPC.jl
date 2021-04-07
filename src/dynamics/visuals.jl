d63365c6036fe8477ab46b46eb39aa85a7b0457f
simon-lc <simonlc@stanford.edu>
2021-04-07 10:34:05 -0700

    2d and 3d surface vis

--- src/dynamics/visuals.jl
+++ src/dynamics/visuals.jl
@@ -27,11 +27,38 @@ function default_background!(vis)
     setvisible!(vis["/Axes"], false)
 end
 
-function plot_surface!(vis, env::Environment{R3})
+function plot_surface!(vis::Visualizer, env::Environment{R3};  col=zeros(3), α=0.4, n::Int=50)
     f(x) = x[3] - env.surf(x[1:2])
+    xlims = [-1.0, 5.0]
+    ylims = [-2.0, 2.0]
+    plot_surface!(vis, f, xlims=xlims, ylims=ylims, col=col, α=α, n=n)
+    return nothing
+end
+
+function plot_surface!(vis::Visualizer, env::Environment{R2};  col=zeros(3), α=0.4, n::Int=50)
+    f(x) = x[3] - env.surf(x[1:1])
+    xlims = [-1.0, 5.0]
+    ylims = [-0.1, 0.1]
+    plot_surface!(vis, f, xlims=xlims, ylims=ylims, col=col, α=α, n=n)
+    return nothing
+end
+
+function plot_surface!(vis::Visualizer, f::Any; xlims = [-1.0, 5.0],
+        ylims = [-0.1, 0.1], col=zeros(3), α=0.4, n::Int=50)
     mesh = GeometryBasics.Mesh(f,
-    	Rect(Vec(-2.0, -2.0, -2.0), Vec(4.0, 4.0, 4.0)),
-        MarchingCubes(), samples=(50, 50, 50))
+    	HyperRectangle(Vec(xlims[1], ylims[1], -2.0), Vec(xlims[2]-xlims[1], ylims[2]-ylims[1], 4.0)),
+        MarchingCubes(), samples=(n, n, n))
     setobject!(vis["surface"], mesh,
-    		   MeshPhongMaterial(color=RGBA{Float32}(1.0, 0.0, 0.0, 0.1)))
+    		   MeshPhongMaterial(color=RGBA{Float32}(col..., α)))
+    return nothing
+end
+
+function get_line_material(size::Real)
+    orange_col = [1,153/255,51/255]
+    blue_col = [51/255,1,1]
+    black_col = [0,0,0]
+    orange_mat = LineBasicMaterial(color=color=RGBA(orange_col...,1.0), linewidth=size)
+    blue_mat = LineBasicMaterial(color=color=RGBA(blue_col...,1.0), linewidth=size)
+    black_mat = LineBasicMaterial(color=color=RGBA(black_col...,1.0), linewidth=size)
+    return orange_mat, blue_mat, black_mat
 end
