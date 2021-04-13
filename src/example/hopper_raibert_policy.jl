986aabc5140d2a26b128f977d9d5c147e253b5f9
simon-lc <simonlc@stanford.edu>
2021-04-13 09:10:57 -0700

    finished raibert policy

--- src/example/hopper_raibert_policy.jl
+++ src/example/hopper_raibert_policy.jl
@@ -6,23 +6,10 @@ open(vis)
 # get hopper model
 model_sim = get_model("hopper_2D", surf="sinusoidal")
 model = get_model("hopper_2D", surf="flat")
-nq = model.dim.q
-nu = model.dim.u
-nc = model.dim.c
-nb = model.dim.b
-nd = nq + nc + nb
-nr = nq + nu + nc + nb + nd
-
-nz = num_var(model)
-nθ = num_data(model)
-r0 = rand(nz)
-z0 = rand(nz)
-θ0 = rand(nθ)
-
-
-# get trajectory
-# ref_traj = get_trajectory("hopper_2D", "gait_in_place", load_type=:joint_traj)
-ref_traj = get_trajectory("hopper_2D", "gait_forward", load_type=:joint_traj)
+nq = model_sim.dim.q
+nu = model_sim.dim.u
+nc = model_sim.dim.c
+nb = model_sim.dim.b
 
 # time
 H = ref_traj.H
@@ -30,38 +17,19 @@ h = ref_traj.h
 N_sample = 5
 H_mpc = 10
 h_sim = h / N_sample
-H_sim = 8000
+H_sim = 3000
 
-# barrier parameter
-κ_mpc = 1.0e-4
+# Select the initial speed
+v0 = 1.9
+off0 = SVector{nq,T}([0.0, 0.5, 0.0, 0.0])
+off1 = SVector{nq,T}([v0*h_sim, 0.5, 0.0, 0.0])
+q_ref = SVector{nq,T}([0.0, 0.5, 0.0, 0.5])
+q0_sim = copy(q_ref) + off0
+q1_sim = copy(q_ref) + off1
 
-cost = CostFunction(H_mpc, model.dim,
-    q = [Diagonal(1.0e-1 * [0.1,3,1,3])   for t = 1:H_mpc],
-    u = [Diagonal(1.0e-0 * [1e-3, 1e0]) for t = 1:H_mpc],
-    γ = [Diagonal(1.0e-100 * ones(model.dim.c)) for t = 1:H_mpc],
-    b = [Diagonal(1.0e-100 * ones(model.dim.b)) for t = 1:H_mpc])
-
-p = linearized_mpc_policy(ref_traj, model, cost,
-    H_mpc = H_mpc,
-    N_sample = N_sample,
-    κ_mpc = κ_mpc,
-    n_opts = NewtonOptions(
-        r_tol = 3e-4,
-        max_iter = 5),
-    mpc_opts = LinearizedMPCOptions(
-        # live_plotting=true,
-        altitude_update = true,
-        altitude_impact_threshold = 0.05,
-        altitude_verbose = true,
-        )
-    )
-
-
-q1_ref = copy(ref_traj.q[2])
-q0_ref = copy(ref_traj.q[1])
-q1_sim = SVector{model.dim.q}(q1_ref)
-q0_sim = SVector{model.dim.q}(copy(q1_sim - (q1_ref - q0_ref) / N_sample))
-@assert norm((q1_sim - q0_sim) / h_sim - (q1_ref - q0_ref) / h) < 1.0e-8
+Tstance = 0.13 # measure using hop-in-place gait
+Tflight = 0.62 # measure using hop-in-place gait
+p = raibert_policy(q0_sim, q1_sim, v0=v0, Tstance=Tstance, Tflight=Tflight)
 
 sim = simulator(model_sim, q0_sim, q1_sim, h_sim, H_sim,
     p = p,
@@ -86,13 +54,16 @@ plot!(plt[2,1], hcat(Vector.(vcat([fill(ref_traj.u[i][1:nu], N_sample) for i=1:H
 plot!(plt[2,1], hcat(Vector.([u[1:nu] for u in sim.traj.u]*N_sample)...)', color=:blue, linewidth=1.0)
 plot!(plt[3,1], hcat(Vector.([γ[1:nc] for γ in sim.traj.γ]*N_sample)...)', color=:blue, linewidth=1.0)
 
-plot_lines!(vis, model, sim.traj.q[1:10:end])
+plot_lines!(vis, model_sim, sim.traj.q[1:10:end])
 plot_surface!(vis, model_sim.env, n=200)
 anim = visualize_robot!(vis, model_sim, sim.traj, sample=20)
 anim = visualize_force!(vis, model_sim, sim.traj, anim=anim, h=h_sim, sample=20)
 
+plot(hcat([q[[2,4]] for q in Vector.(sim.traj.q)[220:530]]...)')
+plot(hcat([q[[3]] for q in Vector.(sim.traj.q)[1:end]]...)')
 
-# filename = "hopper_2d_steep_sine"
+
+# filename = "raibert_hopper_sine"
 # MeshCat.convert_frames_to_video(
 #     "/home/simon/Downloads/$filename.tar",
 #     "/home/simon/Documents/$filename.mp4", overwrite=true)
@@ -100,5 +71,3 @@ anim = visualize_force!(vis, model_sim, sim.traj, anim=anim, h=h_sim, sample=20)
 # convert_video_to_gif(
 #     "/home/simon/Documents/$filename.mp4",
 #     "/home/simon/Documents/$filename.gif", overwrite=true)
-
-# const ContactControl = Main
