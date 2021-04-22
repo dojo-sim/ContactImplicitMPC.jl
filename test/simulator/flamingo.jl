cc217b5d85cbaa8341f8995ce74665102b987223
thowell <thowell@stanford.edu>
2021-04-22 10:13:10 -0700

    flamingo pd

--- test/simulator/flamingo.jl
+++ test/simulator/flamingo.jl
@@ -5,7 +5,7 @@ model.μ_world = 0.1
 ref_traj = deepcopy(ContactControl.get_trajectory("flamingo", "gait0", load_type = :split_traj_alt))
 update_friction_coefficient!(ref_traj, model)
 
-for t = 1:T
+for t = 1:ref_traj.H
 	r = ContactControl.residual(model, ref_traj.z[t], ref_traj.θ[t], 0.0)
 	@test norm(r) < 1.0e-4
 end
@@ -16,12 +16,62 @@ q1 = SVector{model.dim.q}(ref_traj.q[2])
 
 h = ref_traj.h
 H = ref_traj.H
+model.μ_world = 0.9
+
+"""
+    PD tracking policy
+"""
+mutable struct PD2 <: Policy
+	model::ContactDynamicsModel
+    traj::ContactTraj
+	q0::AbstractVector
+    idx::Int
+    cnt::Int
+    N_sample::Int
+end
+
+function pd_policy(model, traj; N_sample = 1)
+    PD2(model, traj, copy(traj.q[1]), 0, N_sample, N_sample)
+end
+
+function policy(p::PD2, x, traj, t)
+    # reset
+    if t == 1
+        p.idx = 0
+        p.cnt = p.N_sample
+    end
+
+    if p.cnt == p.N_sample
+        p.idx += 1
+        p.cnt = 0
+		p.q0 .= copy(x)
+    end
+
+    p.cnt += 1
+
+	u = p.traj.u[p.idx]
+	# @show u
+	# PD
+	kp = 1.0 * ones(p.model.dim.u)
+	kp[1] *= 100.0
+	# kd = 10.0
+
+	# u = Diagonal(kp) * B_func(p.model, x) * (x - p.traj.q[p.idx + 1])
+	# @show u
+	# u -= kd * B_func(p.model, x) * (x - traj.q[t]) / traj.h
+
+    return u ./ p.N_sample
+end
+
+p = pd_policy(model, ref_traj)
 
 # simulator
-sim = ContactControl.simulator(model, q0, q1, h, 40,
-    p = ContactControl.open_loop_policy([SVector{model.dim.u}(ut) for ut in ref_traj.u]),
+sim = ContactControl.simulator(model, q0, q1, h, 24,
+    p = p,
     ip_opts = ContactControl.InteriorPointOptions(
-		r_tol = 1.0e-8, κ_tol = 1.0e-8, κ_init = 1.0e-5, solver = :lu_solver),
+		r_tol = 1.0e-8,
+		κ_init = 1.0e-8,
+		κ_tol = 2.0e-8),
     sim_opts = ContactControl.SimulatorOptions(warmstart = true))
 
 # simulate
@@ -30,7 +80,7 @@ status = ContactControl.simulate!(sim, verbose = false)
 # @test norm(ref_traj.q[end][1:3] - sim.traj.q[end][1:3], Inf) < 0.15
 # end
 
-include(joinpath(pwd(), "src", "dynamics", "flamingo", "visuals.jl"))
-vis = Visualizer()
-render(vis)
+# include(joinpath(pwd(), "src", "dynamics", "flamingo", "visuals.jl"))
+# vis = Visualizer()
+# render(vis)
 anim = visualize_robot!(vis, model, sim.traj)
