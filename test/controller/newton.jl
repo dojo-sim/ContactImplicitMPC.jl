1f93c24f01a228d218fd312ae5e9e3f0564e69d0
thowell <thowell@stanford.edu>
2021-04-27 10:06:42 -0700

    working on tests

--- test/controller/newton.jl
+++ test/controller/newton.jl
@@ -1,121 +1,3 @@
-@testset "Newton: update_traj!" begin
-    # Test set_traj!
-	T = Float64
-	κ = 1.0e-4
-	model = ContactControl.get_model("quadruped")
-	model.μ_world = 0.5
-
-	ref_traj = deepcopy(ContactControl.get_trajectory("quadruped", "gait2", load_type = :split_traj_alt))
-	ContactControl.update_friction_coefficient!(ref_traj, model)
-
-	# time
-	h = ref_traj.h
-	H = ref_traj.H
-
-	# initial conditions
-	q0 = SVector{model.dim.q}(ref_traj.q[1])
-	q1 = SVector{model.dim.q}(ref_traj.q[2])
-
-	function ContactControl.z_initialize!(z, model::ContactControl.Quadruped, q1)
-		nq = model.dim.q
-		z .= 1.0e-1
-		z[1:nq] = q1
-	end
-
-	sim = ContactControl.simulator(model, q0, q1, h, H,
-		p = ContactControl.open_loop_policy(SVector{model.dim.u}.(ref_traj.u)),
-		ip_opts = ContactControl.InteriorPointOptions(
-			r_tol = 1.0e-8, κ_tol = 2κ, κ_init = κ),
-		sim_opts = ContactControl.SimulatorOptions(warmstart = true))
-	ContactControl.simulate!(sim; verbose = false)
-
-	ref_traj = deepcopy(sim.traj)
-	ref_traj1 = deepcopy(ref_traj)
-	for t = 1:H+2
-		ref_traj1.q[t] .+= 1.0e-0 * rand(model.dim.q)
-	end
-
-	im_traj0 = ContactControl.ImplicitTraj(ref_traj, model)
-	im_traj1 = ContactControl.ImplicitTraj(ref_traj1, model)
-
-	ContactControl.implicit_dynamics!(im_traj0, model, ref_traj)
-	@test maximum(norm.(im_traj0.d, 2)) < 5.0e-3
-	ContactControl.implicit_dynamics!(im_traj1, model, ref_traj1)
-	@test maximum(norm.(im_traj1.d, 2)) > 5.0e-1
-	#
-	# # Check that we can optimize z with the residual function r and rz
-	# # Verify that we get the ~same results using r_approx and rz_approx if the linearization was done about the solution.
-	# nq = model.dim.q
-	# nz = ContactControl.num_var(model)
-	# α = 5.0e-2
-	#
-	# ref_traj = deepcopy(sim.traj)
-	# ref_traj1 = deepcopy(ref_traj)
-	# ref_traj1.q[3] .+= α * ones(model.dim.q)
-	# ref_traj1.z[1][1:nq] .= ref_traj1.q[3]
-	#
-	# im_traj0 = ContactControl.ImplicitTraj(ref_traj, model)
-	#
-	# z1 = ref_traj1.z[1]
-	# θ1 = ref_traj1.θ[1]
-	# @test LinearAlgebra.norm(θ1 - ref_traj.θ[1], 1) < 1.0e-8
-	# @test abs(norm(z1 - ref_traj.z[1], 1) - nq * α) < 1.0e-8
-	#
-	# r1 = zeros(nz)
-	# κ = ref_traj1.κ
-	# rz1 = spzeros(nz,nz)
-	# model.res.r!(r1, z1, θ1, κ[1])
-	# @test norm(r1) > 1.0
-	#
-	# #TODO: add tests
-	#
-	# # function dummy_newton(z, θ, κ)
-	# # 	for k = 1:400
-	# # 		r = zeros(nz)
-	# # 		rz = spzeros(nz,nz)
-	# # 		rz = similar(model.spa.rz_sp)
-	# # 		model.res.r!(r, z, θ, κ, nothing)
-	# # 		model.res.rz!(rz, z, θ, nothing)
-	# # 		Δ = - rz \ r
-	# # 		z = z + 0.1 * Δ
-	# # 		# @show norm(r)
-	# # 	end
-	# # 	return z
-	# # end
-	# #
-	# # z2 = dummy_newton(z1, θ1, κ[1])
-	# # model.res.r!(r1, z2, θ1, κ[1], nothing)
-	# # @test norm(r1) < 1.0e-10
-	# #
-	# # function dummy_linear_newton(im_traj, z, θ, κ)
-	# # 	for k = 1:400
-	# # 		r = zeros(nz)
-	# # 		rz = spzeros(nz, nz)
-	# # 		rz = similar(model.spa.rz_sp)
-	# # 		model.linearized.r!(im_traj.lin[1], r, z, θ, κ[1])
-	# # 		model.linearized.rz!(im_traj.lin[1], rz, z, θ)
-	# # 		Δ = - rz \ r
-	# # 		z = z + 0.1 * Δ
-	# # 		# @show norm(r)
-	# # 	end
-	# # 	return z
-	# # end
-	# #
-	# # z3 = dummy_linear_newton(im_traj0, z1, θ1, κ)
-	# # model.linearized.r!(im_traj0.lin[1], r1, z3, θ1, κ[1])
-	# #
-	# # @test norm(r1) < 1.0e-10
-	# #
-	# # # We recover the original z using r and rz
-	# # @test norm(ref_traj.z[1] - z2) < 1.0e-6
-	# #
-	# # # We recover the original z using r_approx and rz_approx
-	# # @test norm(ref_traj.z[1] - z3) < 1.0e-6
-	# #
-	# # # We recover the same solution using either methods
-	# # @test norm(z2 - z3) < 1.0e-6
-end
-
 @testset "Newton: copy_traj!" begin
 	# test copy_traj!
 	model = ContactControl.get_model("quadruped")
@@ -240,7 +122,7 @@ end
 @testset "Newton: residual!" begin
 	model = ContactControl.get_model("quadruped")
 	κ = 1.0e-4
-	ref_traj = ContactControl.get_trajectory("quadruped", "gait0")
+	ref_traj = get_trajectory("quadruped", "gait2", load_type = :split_traj_alt)
 	ref_traj.κ .= κ
 	H = ref_traj.H
 	h = 0.1
@@ -294,4 +176,8 @@ end
 		.+ core.ν[1][1], Inf) < 1.0e-8)
 	@test (norm(core.res.b1[1] .- core.obj.b[1] * core.Δb[1]
 		.+ core.ν[1][1], Inf) < 1.0e-8)
+end
+
+@testset "Newton: newton_solve!" begin
+	
 end
--- test/simulator/quadruped.jl
+++ test/simulator/quadruped.jl
@@ -60,4 +60,36 @@ sim = ContactControl.simulator(model, q0, q1, h, T,
 # simulate
 @test status = ContactControl.simulate!(sim, verbose = false)
 @show sim.traj.q[end][1:3]
-@test norm(ref_traj.q[end][1:3] - sim.traj.q[en
+@test norm(ref_traj.q[end][1:3] - sim.traj.q[end][1:3]) < 0.01
+
+# time
+h = ref_traj.h
+H = ref_traj.H
+
+# initial conditions
+q0 = SVector{model.dim.q}(ref_traj.q[1])
+q1 = SVector{model.dim.q}(ref_traj.q[2])
+
+function ContactControl.z_initialize!(z, model::ContactControl.Quadruped, q1)
+    nq = model.dim.q
+    z .= 1.0e-1
+    z[1:nq] = q1
+end
+
+ref_traj1 = deepcopy(ref_traj)
+for t = 1:H+2
+    ref_traj1.q[t] .+= 1.0e-0 * rand(model.dim.q)
+end
+
+im_traj0 = ContactControl.ImplicitTraj(ref_traj, model)
+im_traj1 = ContactControl.ImplicitTraj(ref_traj1, model)
+
+ContactControl.implicit_dynamics!(im_traj0, model, ref_traj)
+
+im_traj0.ip[1].r
+ref_traj.b[1]
+im_traj0.db1[1]
+norm(im_traj0.d[1])
+@test maximum([norm(d) for d in im_traj0.d]) < 5.0e-3
+ContactControl.implicit_dynamics!(im_traj1, model, ref_traj1)
+@test maximum(norm.(im_traj1.d, 2)) > 5.0e-1
