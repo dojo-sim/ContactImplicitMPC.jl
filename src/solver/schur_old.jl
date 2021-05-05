2bc186cb52a64ba8a5e0b373629b1c4611483032
simon-lc <simonlc@stanford.edu>
2021-05-05 00:09:10 -0700

    30% improvement on Schur solver

--- src/solver/schur_old.jl
+++ src/solver/schur_old.jl
@@ -16,8 +16,6 @@ mutable struct Schur{T,n,m,nn,nm,mm}
     C::SMatrix{m,n,T,nm}
     D::SMatrix{m,m,T,mm}
     Ai::SMatrix{n,n,T,nn} # Inverse of A
-    CAi::SMatrix{m,n,T,nm} # C*A^{-1}
-    CAiB::SMatrix{m,m,T,mm} # C*A^{-1}*B
     gs_data::SDMGSData{m,T} # Schur complement of the block A in M
     u::SVector{n,T}
     v::SVector{m,T}
@@ -32,15 +30,13 @@ function Schur(M::AbstractMatrix{T}; n::Int=1, m::Int=size(M)[1]-n) where {T}
     C = SMatrix{m,n,T,n*m}(M[n .+ (1:m),1:n])
     D = SMatrix{m,m,T,m^2}(M[n .+ (1:m),n .+ (1:m)])
     Ai = inv(A)
-    CAi = C*Ai
-    CAiB = C*Ai*B
     gs_data = SDMGSData(m,T=T)
-    gs!(gs_data, D - CAiB)
+    gs!(gs_data, D - C*Ai*B)
     u = zeros(SVector{n,T})
     v = zeros(SVector{m,T})
     x = zeros(SVector{n,T})
     y = zeros(SVector{m,T})
-    return Schur{T,n,m,n^2,n*m,m^2}(A,B,C,D,Ai,CAi,CAiB,gs_data,u,v,x,y)
+    return Schur{T,n,m,n^2,n*m,m^2}(A,B,C,D,Ai,gs_data,u,v,x,y)
 end
 
 """
@@ -48,15 +44,14 @@ end
     It recomputes the Gram-Schmidt factorization of the Schur complement of the block A.
 """
 function schur_update!(S::Schur{T,n,m,nn,nm,mm}, D::AbstractMatrix{T}) where {T,n,m,nn,nm,mm}
-    # update D, gs_data and the shur complement
-    # B = S.B
-    # C = S.C
-    # S.D = D
-    # Ai = S.Ai
-    CAiB = S.CAiB
+    # update D and gs_data, the shur complement
+    B = S.B
+    C = S.C
+    S.D = D
+    Ai = S.Ai
     gs_data = S.gs_data
 
-    gs!(gs_data, D - CAiB)
+    gs!(gs_data, D - C*(Ai*B))
     return nothing
 end
 
@@ -64,9 +59,11 @@ end
     Solve for [x;y] given [u,v] and the factorized matrix M.
 """
 function schur_solve!(S::Schur{T,n,m,nn,nm,mm}, u::AbstractVector{T}, v::AbstractVector{T}) where {T,n,m,nn,nm,mm}
+    A  = S.A
     B  = S.B
+    C  = S.C
+    D  = S.D
     Ai = S.Ai
-    CAi = S.CAi
     gs_data = S.gs_data
     S.u = u
     S.v = v
@@ -74,7 +71,7 @@ function schur_solve!(S::Schur{T,n,m,nn,nm,mm}, u::AbstractVector{T}, v::Abstrac
     vs  = S.v
     # S.x = Ai*(us + B*(As*(C*(Ai*us) - vs)))
     # S.y = As*(- C*(Ai*us) + vs)
-    qr_solve!(gs_data, CAi*us - vs)
+    qr_solve!(gs_data, C*(Ai*us) - vs)
     temp = gs_data.xs
     S.x = Ai*(us + B*temp)
     S.y = - temp
