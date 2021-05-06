ff6da0ae0b6ed26c6f12b98f052552c294f61834
thowell <thowell@stanford.edu>
2021-05-05 22:12:01 -0700

    stairs visual for hopper

--- src/dynamics/hopper_2D/visuals.jl
+++ src/dynamics/hopper_2D/visuals.jl
@@ -69,3 +69,17 @@ function contact_point(model::Hopper2D, q::AbstractVector)
 	pc = [p_foot]
 	return pc
 end
+
+function plot_stairs!(vis)
+	setobject!(vis["box1"], GeometryBasics.HyperRectangle(Vec(0.0, 0.0, 0.0),
+	Vec(0.5, 0.5, 0.25)), MeshPhongMaterial(color = RGBA(0.5, 0.5, 0.5, 1.0)))
+	settransform!(vis["box1"], Translation(0.25, -0.25, 0))
+
+	setobject!(vis["box2"], GeometryBasics.HyperRectangle(Vec(0.0, 0.0, 0.0),
+		Vec(0.5, 0.5, 2 * 0.25)), MeshPhongMaterial(color = RGBA(0.5, 0.5, 0.5, 1.0)))
+	settransform!(vis["box2"], Translation(0.25 + 0.5, -0.25, 0))
+
+	setobject!(vis["box3"], GeometryBasics.HyperRectangle(Vec(0.0, 0.0, 0.0),
+		Vec(0.5, 0.5, 3 * 0.25)), MeshPhongMaterial(color = RGBA(0.5, 0.5, 0.5, 1.0)))
+	settransform!(vis["box3"], Translation(0.25 + 2 * 0.5, -0.25, 0))
+end
