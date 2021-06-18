7abeedbbe2b759a34876bd72a09278978b28edd8
thowell <thowell@stanford.edu>
2021-06-17 17:35:16 -0700

    newton configuration only

--- src/examples/flamingo.jl
+++ src/examples/flamingo.jl
@@ -17,7 +17,7 @@ h = ref_traj.h
 N_sample = 5
 H_mpc = 15
 h_sim = h / N_sample
-H_sim = 12000#35000
+H_sim = 2500#12000#35000
 
 # barrier parameter
 κ_mpc = 1.0e-4
@@ -33,9 +33,9 @@ H_sim = 12000#35000
 obj = TrackingVelocityObjective(model, env, H_mpc,
     v = [Diagonal(1e-3 * [1e0,1,1e4,1,1,1,1,1e4,1e4]) for t = 1:H_mpc],
     q = [Diagonal(1e-1 * [3e2, 1e-6, 3e2, 1, 1, 1, 1, 0.1, 0.1]) for t = 1:H_mpc],
-    u = [Diagonal(3e-1 * [0.1; 0.1; 0.3; 0.3; ones(model.dim.u-6); 2; 2]) for t = 1:H_mpc],
-    γ = [Diagonal(1.0e-100 * ones(model.dim.c)) for t = 1:H_mpc],
-    b = [Diagonal(1.0e-100 * ones(model.dim.c * friction_dim(env))) for t = 1:H_mpc])
+    u = [Diagonal(3e-1 * [0.1; 0.1; 0.3; 0.3; ones(model.dim.u-6); 2; 2]) for t = 1:H_mpc])
+#   γ = [Diagonal(1.0e-100 * ones(model.dim.c)) for t = 1:H_mpc],
+#   b = [Diagonal(1.0e-100 * ones(model.dim.c * friction_dim(env))) for t = 1:H_mpc])
 
 p = linearized_mpc_policy(ref_traj, s, obj,
     H_mpc = H_mpc,
