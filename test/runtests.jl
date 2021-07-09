98caea403f252f1322f17d3d45ccffd2046c566b
simon-lc <simonlc@stanford.edu>
2021-07-09 15:13:56 -0700

    testing flamingo

--- test/runtests.jl
+++ test/runtests.jl
@@ -37,45 +37,46 @@ using Quaternions
 # # include("controller/mpc_flamingo.jl")
 
 
+include("simulator/flamingo.jl") #TODO: add this test
 
-
-# Solver
-include("solver/gs.jl")
-include("solver/qdldl.jl")
-include("solver/random_qp.jl")
-include("solver/lu.jl")
-include("solver/gs.jl")
-include("solver/schur.jl")
-include("solver/soc.jl")
-include("solver/mehrotra.jl")
-
-# Dynamics
-include("dynamics/lagrangian.jl") # need to fix d_fast
-include("dynamics/model.jl")
-include("dynamics/particle.jl")
-include("dynamics/quadruped.jl")
-include("dynamics/quaternion.jl")
-
-# Simulator
-include("simulator/rotations.jl")
-include("simulator/environment.jl")
-include("simulator/trajectory.jl")
-include("simulator/simulator.jl")
-include("simulator/particle.jl")
-include("simulator/hopper_2D.jl") #TODO: set tests to raibert model
-include("simulator/hopper_3D.jl")
-include("simulator/quadruped.jl")
-include("simulator/open_loop.jl")
-# include("simulator/biped.jl") #TODO:  improve this test #NEED FIX
-# include("simulator/flamingo.jl") #TODO: add this test #NEED FIX
-
-# Controller
-include("controller/objective.jl")
-include("controller/linearized_step.jl")
-include("controller/implicit_dynamics.jl")
-include("controller/linearized_solver.jl")
-include("controller/newton.jl")
-
-# MPC examples
-include("controller/mpc_quadruped.jl")
-include("controller/mpc_flamingo.jl")
+#
+# # Solver
+# include("solver/gs.jl")
+# include("solver/qdldl.jl")
+# include("solver/random_qp.jl")
+# include("solver/lu.jl")
+# include("solver/gs.jl")
+# include("solver/schur.jl")
+# include("solver/soc.jl")
+# include("solver/mehrotra.jl")
+#
+# # Dynamics
+# include("dynamics/lagrangian.jl") # need to fix d_fast
+# include("dynamics/model.jl")
+# include("dynamics/particle.jl")
+# include("dynamics/quadruped.jl")
+# include("dynamics/quaternion.jl")
+#
+# # Simulator
+# include("simulator/rotations.jl")
+# include("simulator/environment.jl")
+# include("simulator/trajectory.jl")
+# include("simulator/simulator.jl")
+# include("simulator/particle.jl")
+# include("simulator/hopper_2D.jl") #TODO: set tests to raibert model
+# include("simulator/hopper_3D.jl")
+# include("simulator/quadruped.jl")
+# include("simulator/open_loop.jl")
+# # include("simulator/biped.jl") #TODO:  improve this test #NEED FIX
+# # include("simulator/flamingo.jl") #TODO: add this test
+#
+# # Controller
+# include("controller/objective.jl")
+# include("controller/linearized_step.jl")
+# include("controller/implicit_dynamics.jl")
+# include("controller/linearized_solver.jl")
+# include("controller/newton.jl")
+#
+# # MPC examples
+# include("controller/mpc_quadruped.jl")
+# include("controller/mpc_flamingo.jl")
