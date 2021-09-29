511e1ed8e0fe4d78fd7cde09de23a459f4e36045
thowell <thowell@stanford.edu>
2021-09-29 13:30:43 -0700

    quadruped flat

--- examples/quadruped.jl
+++ examples/quadruped.jl
@@ -1,27 +1,31 @@
-include(joinpath(@__DIR__, "..", "dynamics", "quadruped", "visuals.jl"))
-vis = Visualizer()
+using ContactImplicitMPC 
+using LinearAlgebra 
+using StaticArrays
+
+# Visualizer
+vis = ContactImplicitMPC.Visualizer()
 open(vis)
-const ContactImplicitMPC = Main
 
-
-
+# Simulation
 s = get_simulation("quadruped", "flat_2D_lc", "flat")
 model = s.model
 env = s.env
 
-ref_traj_ = deepcopy(ContactImplicitMPC.get_trajectory(s.model, s.env,
+# Reference Trajectory
+ref_traj = deepcopy(ContactImplicitMPC.get_trajectory(s.model, s.env,
     joinpath(module_dir(), "src/dynamics/quadruped/gaits/gait2.jld2"),
     load_type = :split_traj_alt))
-ref_traj = deepcopy(ref_traj_)
+
 update_friction_coefficient!(ref_traj, model, env)
 
-# time
 H = ref_traj.H
 h = ref_traj.h
+
+# MPC setup
 N_sample = 5
 H_mpc = 10
 h_sim = h / N_sample
-H_sim = 10000 #4000 #3000
+H_sim = 2500 #4000 #3000
 
 # barrier parameter
 κ_mpc = 2.0e-4
@@ -60,13 +64,13 @@ p = linearized_mpc_policy(ref_traj, s, obj,
 					),
     )
 
-
+# Initial conditions
 q1_ref = copy(ref_traj.q[2])
 q0_ref = copy(ref_traj.q[1])
 q1_sim = SVector{model.dim.q}(q1_ref)
 q0_sim = SVector{model.dim.q}(copy(q1_sim - (q1_ref - q0_ref) / N_sample))
-@assert norm((q1_sim - q0_sim) / h_sim - (q1_ref - q0_ref) / h) < 1.0e-8
 
+# Simulator
 sim = simulator(s, q0_sim, q1_sim, h_sim, H_sim,
     p = p,
 	ip_opts = InteriorPointOptions(
@@ -78,7 +82,12 @@ sim = simulator(s, q0_sim, q1_sim, h_sim, H_sim,
     sim_opts = SimulatorOptions(warmstart = true),
     )
 
+# Simulate
 @time status = ContactImplicitMPC.simulate!(sim, verbose = true)
+
+# Visualize
+anim = visualize_meshrobot!(vis, model, sim.traj, sample=5)
+anim = visualize_force!(vis, model, env, sim.traj, anim=anim, h=h_sim, sample = 5)
 
 ################################################################################
 # Timing result
@@ -92,71 +101,4 @@ sim.stats.μ_dt
 H_sim * h_sim / sum(sim.stats.dt)
 
 
-# @elapsed status = ContactImplicitMPC.simulate!(sim)
-# @profiler status = ContactImplicitMPC.simulate!(sim)
 
-plot_lines!(vis, model, sim.traj.q[1:1:end])
-plot_surface!(vis, env, ylims=[0.3, -0.05])
-anim = visualize_meshrobot!(vis, model, sim.traj, sample=5)
-# anim = visualize_robot!(vis, model, sim.traj, anim=anim)
-anim = visualize_force!(vis, model, env, sim.traj, anim=anim, h=h_sim, sample = 5)
-
-# Display ghosts
-t_ghosts = [1, 1333, 2666]
-mvis_ghosts = []
-for (i,t) in enumerate(t_ghosts)
-    α = i/(length(t_ghosts)+1)
-    name = Symbol("ghost$i")
-    mvis = build_meshrobot!(vis, model, name=name, α=α)
-    push!(mvis_ghosts, mvis)
-end
-
-for (i,t) in enumerate(t_ghosts)
-    name = Symbol("ghost$i")
-    set_meshrobot!(vis, mvis_ghosts[i], model, sim.traj.q[t], name=name)
-end
-
-
-
-
-
-# R = Dict{Symbol,Vector}(
-# 	:dyn => [],
-# 	:imp => [],
-# 	:mdp => [],
-# 	:fri => [],
-# 	:bimp => [],
-# 	:bmdp => [],
-# 	:bfri => [],
-# 	)
-# nz = num_var(model, env)
-# dyn = index_dyn(model, env, quat = true)
-# imp = index_imp(model, env, quat = true)
-# mdp = index_mdp(model, env, quat = true)
-# fri = index_fri(model, env, quat = true)
-# bimp = index_bimp(model, env, quat = true)
-# bmdp = index_bmdp(model, env, quat = true)
-# bfri = index_bfri(model, env, quat = true)
-# for t = 1:ref_traj.H
-#
-# 	z = deepcopy(ref_traj.z[t])
-# 	θ = deepcopy(ref_traj.θ[t])
-# 	r = zeros(nz)
-# 	r = residual(model, env, z, θ, 0.0)
-# 	s.res.r!(r, z, θ, 1e-6)
-# 	push!(R[:dyn], norm(r[dyn]))
-# 	push!(R[:imp], norm(r[imp]))
-# 	push!(R[:mdp], norm(r[mdp]))
-# 	push!(R[:fri], norm(r[fri]))
-# 	push!(R[:bimp], norm(r[bimp]))
-# 	push!(R[:bmdp], norm(r[bmdp]))
-# 	push!(R[:bfri], norm(r[bfri]))
-# end
-# plt = plot()
-# plot!(plt, R[:dyn], label = "dyn")
-# plot!(plt, R[:imp], label = "imp")
-# plot!(plt, R[:mdp], label = "mdp")
-# plot!(plt, R[:fri], label = "fri")
-# plot!(plt, R[:bimp], label = "bimp")
-# plot!(plt, R[:bmdp], label = "bmdp")
-# plot!(plt, R[:bfri], label = "bfri")
