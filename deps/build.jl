be29bf71e33af718bad156ceb939cdca44843dcc
thowell <thowell@stanford.edu>
2021-10-01 18:14:34 -0700

    deps

--- deps/build.jl
+++ deps/build.jl
@@ -2,5 +2,5 @@ append!(empty!(LOAD_PATH), Base.DEFAULT_LOAD_PATH)
 using Pkg
 exampledir = joinpath(@__DIR__, "..", "examples")
 Pkg.activate(exampledir)
-Pkg.instantiate()
+Pkg.add("Literate")
 include(joinpath(exampledir, "generate_notebooks.jl"))
